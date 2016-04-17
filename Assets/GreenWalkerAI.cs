dbb731feb6821b2de7fef3f0a8789219bb24d15f
Cosmosquark <bthompson2090@gmail.com>
2016-04-17 11:58:23 +0100

    adding in my changes, lets build this game now :)

---
+++ Assets/GreenWalkerAI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GreenWalkerAI : MonoBehaviour {
+
+    void FixedUpdate()
+    {
+        FPSAI controller = this.GetComponent<FPSAI>();
+        controller.walkSpeed = 0.0f;
+    }
+}
